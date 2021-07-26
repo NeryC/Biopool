a9f59922a625515137e4d744c4e9874ca3c8a220
Nery <nery@oktana.io>
2021-07-26 04:04:30 -0400

    test

--- components/Layout/App-bar/index.tsx
+++ components/Layout/App-bar/index.tsx
@@ -1,11 +1,29 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from '../../../context/globalStore';
 import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 export const AppBar = () => {
+  const router = useRouter()
   const {state} = useContext(Context);
+  const [values, setValues] = useState({
+    launcherId: '',
+  });
+
+  function handleInputChange(e) {
+    setValues((values) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  function _handleKeyDown(e)  {
+    if (e.key === 'Enter') {
+      router.push(`/launcher-id/${values.launcherId}`)
+    }
+  }
 
   return (
     <div className={`${!state.modalIsOpen && 'sticky'} top-0 py-2 bg-white z-10`}>
@@ -15,14 +33,17 @@ export const AppBar = () => {
         </div>
         <div className="relative col-end-9 lg:col-end-13 col-span-3">
           <input type="text" 
-            name="serch"
+            name="launcherId"
             placeholder="Launcher ID"
             autoComplete="off"
             spellCheck="false"
+            value={values.launcherId}
+            onChange={handleInputChange}
+            onKeyDown={_handleKeyDown}
             className="
               h-full
               pl-2
-              pr-8
+              pr-10
               py-1
               w-full 
               rounded
@@ -32,13 +53,16 @@ export const AppBar = () => {
               font-gibson-light 
               text-green1 
               text-sm
-            "/>
-          <button type="submit" className="absolute right-0 top-0 mt-1 md:mt-2 mr-4 text-base">
-            <FontAwesomeIcon
-              icon={faSearch}
-              className="text-lime1 "
-            />
-          </button>
+            "
+          />
+          <Link href={`/launcher-id/${values.launcherId}`} passHref>
+            <button type="submit" className="absolute right-0 top-0 mt-1 md:mt-2 mr-4 text-base">
+              <FontAwesomeIcon
+                icon={faSearch}
+                className="text-lime1 "
+              />
+            </button>
+          </Link>
         </div>
       </div>
     </div>
